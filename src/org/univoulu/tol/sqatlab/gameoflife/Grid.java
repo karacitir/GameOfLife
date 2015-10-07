41ee03c18a83af33146cf85635021b7bd77b55fb
somename <someemail>
2015-10-07 16:06:15 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/univoulu/tol/sqatlab/gameoflife/Grid.java
@@ -3,6 +3,12 @@ package org.univoulu.tol.sqatlab.gameoflife;
 public class Grid {
 
 	private Cell[][] cells;
+	private int size;
+	
+	public Grid(int size) {
+		cells = new Cell[size][size];
+		this.size = size;
+	}
 	
 	public Cell[][] getCells() {
 		return cells;
